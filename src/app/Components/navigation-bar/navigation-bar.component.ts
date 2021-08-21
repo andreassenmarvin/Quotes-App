d9b061ad2b770f0ea53f8c8d71a10181b8dbaeb7
Marvin Maina <machariamarvin625@gmail.com>
2021-08-21 20:51:14 +0300

    Make navbar fixed on scroll

--- src/app/Components/navigation-bar/navigation-bar.component.ts
+++ src/app/Components/navigation-bar/navigation-bar.component.ts
@@ -12,13 +12,25 @@ export class NavigationBarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  @HostListener('window:scroll', ['$event']) onscroll() {
+  navbarscroll: boolean = false;
+  titleblack: boolean = false;
+  logo1hide: boolean = false;
+  logo2show: boolean = false;
+
+  @HostListener('window:scroll', ['$event']) 
+  onscroll() {
     if (window.scrollY > 100) {
-      $('#navbar').addClass("nav")
+      this.navbarscroll = true;
+      this.titleblack = true;
+      this.logo1hide = true;
+      this.logo2show = true;
     }
 
     else {
-      
+      this.navbarscroll = false;
+      this.titleblack = false;
+      this.logo1hide = false;
+      this.logo2show = false;
     }
   }
 }
