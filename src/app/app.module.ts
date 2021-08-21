eecd561e975ec86b9f2348e7db6e7f0c087a8e23
Marvin Maina <machariamarvin625@gmail.com>
2021-08-21 21:28:06 +0300

    Add create task button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header-bg/header.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
+import { HeaderBtnComponent } from './Components/header-btn/header-btn.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavigationBarComponent
+    NavigationBarComponent,
+    HeaderBtnComponent
   ],
   imports: [
     BrowserModule,
