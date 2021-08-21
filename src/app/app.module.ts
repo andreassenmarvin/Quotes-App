83344807787d6d4d780bb952269cf71125f6c6fe
Marvin Maina <machariamarvin625@gmail.com>
2021-08-21 22:00:06 +0300

    Add header quote card component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header-bg/header.component';
 import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
 import { HeaderBtnComponent } from './Components/header-btn/header-btn.component';
+import { HeaderCardComponent } from './Components/header-card/header-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavigationBarComponent,
-    HeaderBtnComponent
+    HeaderBtnComponent,
+    HeaderCardComponent
   ],
   imports: [
     BrowserModule,
