d4156d8dc0a47716cd2d731f1f94de3d2596d39f
Marvin Maina <machariamarvin625@gmail.com>
2021-08-21 15:50:32 +0300

    Add nav-bar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './Components/header-bg/header.component';
+import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
